717ae0a6f2f3cfca9f4187de6fd1c82be46f9762
Lucas Mafra <lucas.mafra.barros@usp.br>
2018-03-02 00:43:00 -0300

    condition expression working

--- playground/test.ts
+++ playground/test.ts
@@ -1,9 +1,9 @@
 import User from './User'
 
 async function test() {
-    const users = await User.onIndex.otherIndex
-                        .scan().withAttributes(['name']).allResults()
-    console.log(users.data)
+    const users = await User
+                        .scan().withAttributes(['id']).allResults()
+    console.log(users)
 }
 
 test()
