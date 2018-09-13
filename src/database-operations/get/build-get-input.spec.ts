5ae009d0cbf56e4dca3eed58fb08a515eccc55bc
Lucas Mafra <lucas.mafra95@gmail.com>
2018-09-13 20:51:53 -0300

    created spec for build get input

---
+++ src/database-operations/get/build-get-input.spec.ts
@@ -0,0 +1,35 @@
+import { buildGetInput } from './build-get-input'
+import { get } from './get'
+
+describe('buildGetInput', () => {
+  const schema = { schema: { tableName: 'DummyTable' }, key: { id: '1' } }
+
+  it('builds get input from schema', () => {
+    expect(buildGetInput(schema as any)).toEqual({
+      TableName: 'DummyTable',
+      Key: {
+        id: {
+          S: '1',
+        },
+      },
+    })
+  })
+
+  context('when withAttributes option is present', () => {
+    const withAttributes = {
+      attributes: ['#id', '#name', '#email'],
+      expressionAttributeNames: {
+        '#id': 'id', '#name': 'name', '#email': 'email',
+      },
+    }
+
+    it('builds projection expression', () => {
+      expect(buildGetInput(schema as any, withAttributes)).toMatchObject({
+        ProjectionExpression: '#id,#name,#email',
+        ExpressionAttributeNames: {
+          '#id': 'id', '#name': 'name', '#email': 'email',
+        },
+      })
+    })
+  })
+})
--- src/database-operations/get/build-get-input.ts
+++ src/database-operations/get/build-get-input.ts
@@ -3,17 +3,17 @@ import { WithAttributes } from '../../chaining/common'
 import { Get } from '../../chaining/find/get/get'
 import { buildKey, projectionExpression } from '../helpers'
 
-export function buildGetInput<KeySchema>(
-    get: Get<KeySchema>,
-    withAttributes?: WithAttributes,
-): DynamoDB.GetItemInput {
-    const input: DynamoDB.GetItemInput = {
-        TableName: get.schema.tableName,
-        Key: buildKey(get.key),
-    }
-    if (withAttributes) {
-        input.ProjectionExpression = projectionExpression(withAttributes.attributes)
-        input.ExpressionAttributeNames = withAttributes.expressionAttributeNames
-    }
-    return input
+export const buildGetInput = <KeySchema>(
+  get: Get<KeySchema>,
+  withAttributes?: WithAttributes,
+): DynamoDB.GetItemInput => {
+  const input: DynamoDB.GetItemInput = {
+    TableName: get.schema.tableName,
+    Key: buildKey(get.key),
+  }
+  if (withAttributes) {
+    input.ProjectionExpression = projectionExpression(withAttributes.attributes)
+    input.ExpressionAttributeNames = withAttributes.expressionAttributeNames
+  }
+  return input
 }
