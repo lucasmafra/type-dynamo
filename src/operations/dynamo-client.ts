2776969a954cf900978aeb52436d7e0ad68b166a
Lucas Mafra <lucas.mafra95@gmail.com>
2018-09-30 23:14:44 -0300

    using promisify in dynamo-client

--- src/operations/dynamo-client.ts
+++ src/operations/dynamo-client.ts
@@ -1,121 +1,54 @@
-import * as AWS from 'aws-sdk'
 import { DynamoDB } from 'aws-sdk'
+import { promisify } from 'util'
 
 class DynamoClient {
 
-  constructor(private dynamo: DynamoDB.DocumentClient) {
-  }
+  constructor(private dynamo: DynamoDB.DocumentClient) { }
 
   public async scan(params: DynamoDB.ScanInput): Promise<DynamoDB.ScanOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.scan(params,
-        (err: AWS.AWSError, data: DynamoDB.DocumentClient.ScanOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.scan)(params)
   }
 
   public async batchGet(
     params: DynamoDB.BatchGetItemInput,
   ): Promise<DynamoDB.BatchGetItemOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.batchGet(params,
-        (err: AWS.AWSError, data: DynamoDB.BatchGetItemOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.batchGet)(params)
   }
 
   public async getItem(
     params: DynamoDB.GetItemInput,
   ): Promise<DynamoDB.GetItemOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.get(params,
-        (err: AWS.AWSError, data: DynamoDB.DocumentClient.GetItemOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.get)(params)
   }
 
   public async put(
     params: DynamoDB.PutItemInput,
   ): Promise<DynamoDB.PutItemOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.put(params,
-        (err: AWS.AWSError, data: DynamoDB.DocumentClient.GetItemOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.put)(params)
   }
 
   public async update(
     params: DynamoDB.UpdateItemInput,
   ): Promise<DynamoDB.UpdateItemOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.update(params,
-        (err: AWS.AWSError, data: DynamoDB.DocumentClient.UpdateItemOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.update)(params)
   }
 
   public async delete(
     params: DynamoDB.DeleteItemInput,
   ): Promise<DynamoDB.DeleteItemOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.delete(params,
-        (err: AWS.AWSError, data: DynamoDB.DocumentClient.DeleteItemOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.delete)(params)
   }
 
   public async query(
     params: DynamoDB.QueryInput,
   ): Promise<DynamoDB.QueryOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.query(params,
-        (err: AWS.AWSError, data: DynamoDB.DocumentClient.QueryOutput) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.query)(params)
   }
 
   public async batchWrite(
     params: DynamoDB.BatchWriteItemInput,
   ): Promise<DynamoDB.BatchWriteItemOutput> {
-    return new Promise((resolve, reject) => {
-      this.dynamo.batchWrite(params,
-        (err: AWS.AWSError,
-         data: DynamoDB.DocumentClient.BatchWriteItemOutput,
-        ) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
-    })
+    return promisify(this.dynamo.batchWrite)(params)
   }
 }
 
