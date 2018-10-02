83df78bdaad1f2f44cbdb0a0d32ee0cd8e67db11
Lucas Mafra <lucas.mafra95@gmail.com>
2018-10-02 15:01:12 -0300

    configured typeDynamo environment on acceptance test

--- features/support/utils/type-dynamo.ts
+++ features/support/utils/type-dynamo.ts
@@ -1,6 +1,8 @@
 import { TypeDynamo } from '../../../src'
 
 export const typeDynamo = new TypeDynamo({
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.secret_access_key,
   region: 'localhost',
   endpoint: 'http://localhost:8000',
 })
